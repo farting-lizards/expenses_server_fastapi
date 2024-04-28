d328f5a658cd03ba8561fec9229a0abc343faded
Dinika Saxena <dinikasaxenas@gmail.com>
2024-04-28 17:47:41 +0200

    Populate categories and accounts

--- expenses_server/main.py
+++ expenses_server/main.py
@@ -2,18 +2,10 @@ from fastapi import APIRouter, FastAPI
 
 from .routes.accounts import router as account_router
 from .routes.expenses import router as expense_router
-from sqlalchemy import event
-from .db_models.account import Account
-from .db_models.category import Category
-from .seed_db import populate_accounts, populate_categories
 
 
 app = FastAPI()
 base_router = APIRouter(prefix="/api")
-
-
-event.listen(Category.__table__, "after_create", populate_categories)
-event.listen(Account.__table__, "after_create", populate_accounts)
 
 
 base_router.get("/")
