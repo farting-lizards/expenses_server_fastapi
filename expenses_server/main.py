3e3ac8cd6f2dd3b46fc8e22464d0168b7dae52f2
Dinika Saxena <dinikasaxenas@gmail.com>
2024-07-30 18:33:56 +0200

    Change token route to api/users/token to be consistent with other endpoints

--- expenses_server/main.py
+++ expenses_server/main.py
@@ -1,23 +1,14 @@
-from http import HTTPStatus
-from fastapi import APIRouter, Depends, FastAPI, HTTPException
+from fastapi import APIRouter, FastAPI
 from fastapi.responses import FileResponse
-from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
 from passlib.context import CryptContext
-from typing import Annotated
-from sqlalchemy.orm import Session
 
-from expenses_server.db_models.user import User
-from expenses_server.dtos.users import UserToken
-from expenses_server.utils import hash_password, verify_password
 
 from .settings import settings
 
 from .routes.accounts import router as account_router
 from .routes.expenses import router as expense_router
 from .routes.users import router as user_router
-from expenses_server.db import get_db
-from sqlalchemy.exc import NoResultFound
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -28,31 +19,6 @@ app = FastAPI()
 async def root() -> FileResponse:
     response = FileResponse(f"{settings.frontend_path}/index.html")
     return response
-
-
-@app.post("/token")
-async def login(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db_session: Session = Depends(get_db),
-) -> UserToken:
-    try:
-        user = (
-            db_session.query(User)
-            .where(
-                User.username == form_data.username,
-            )
-            .one()
-        )
-        if not verify_password(form_data.password, user.password_hash):
-            raise HTTPException(
-                status_code=HTTPStatus.BAD_REQUEST,
-                detail="Incorrect username or password",
-            )
-        return UserToken(access_token=user.username, token_type="bearer")
-    except NoResultFound:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Incorrect username or password"
-        )
 
 
 base_router = APIRouter(prefix="/api")
--- expenses_server/routes/users.py
+++ expenses_server/routes/users.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import NoResultFound
 from expenses_server.db import get_db
 from expenses_server.db_models.user import User
 from expenses_server.dtos.users import UserToken, get_current_user
-from expenses_server.utils import hash_password
+from expenses_server.utils import verify_password
 
 
 router = APIRouter(prefix="/users")
@@ -20,3 +20,28 @@ async def read_user_current(
 ) -> User:
     # TODO: Only accounts for user should be sent
     return current_user
+
+
+@router.post("/token")
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db_session: Session = Depends(get_db),
+) -> UserToken:
+    try:
+        user = (
+            db_session.query(User)
+            .where(
+                User.username == form_data.username,
+            )
+            .one()
+        )
+        if not verify_password(form_data.password, user.password_hash):
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail="Incorrect username or password",
+            )
+        return UserToken(access_token=user.username, token_type="bearer")
+    except NoResultFound:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="Incorrect username or password"
+        )
