e363ffe4c56c1391e7eda568afa185b2a504dd8d
Dinika Saxena <dinikasaxenas@gmail.com>
2024-09-04 23:38:38 +0200

    Add oauth2 for login

--- expenses_server/dtos/users.py
+++ expenses_server/dtos/users.py
@@ -1,9 +1,4 @@
-from typing import Annotated
-from uuid import uuid4
-from fastapi import Depends
 from pydantic import UUID4, BaseModel
-
-from ..security import oauth2_scheme
 
 
 class User(BaseModel):
@@ -14,12 +9,3 @@ class User(BaseModel):
 class UserToken(BaseModel):
     access_token: str
     token_type: str
-
-
-def fake_decode_token(token: str) -> User:
-    return User(id=uuid4(), username=f"{token} fakedecoded")
-
-
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
-    user = fake_decode_token(token)
-    return user
--- expenses_server/settings.py
+++ expenses_server/settings.py
@@ -13,11 +13,9 @@ class Settings(BaseSettings):
     hash_algorithm: str = "sha256"
     password_seed: str = "dummy_seed"
 
-    jwt_encode_key: str = (
-        "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    )
+    jwt_encode_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # TODO: Generate correctly
     jwt_algorithm: str = "HS256"
-    jwt_token_expire_minutes: int = 1
+    jwt_token_expire_minutes: int = 1  # TODO: Longer expiration time
 
 
 settings = Settings()
