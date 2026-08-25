b16ee18e5d2e278f68415a43bea21f3c263e34f9
Dinika Saxena <dinika@greyllama.cc>
2026-08-25 17:17:57 +0200

    Setup jwt key for tests

--- expenses_server/settings.py
+++ expenses_server/settings.py
@@ -1,21 +1,21 @@
+import os
+
 from pydantic import PostgresDsn
 from pydantic_core import MultiHostUrl
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=os.environ.get("ENV_FILE", ".env"))
+
     db_url: PostgresDsn = MultiHostUrl(
         "postgresql://expenses:expenses@localhost:15000/expenses"
     )
     frontend_path: str | None = None  # "../expenses-react/build"
 
-    # Password
-    hash_algorithm: str = "sha256"
-    password_seed: str = "dummy_seed"
-
-    jwt_encode_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # TODO: Generate correctly
+    jwt_encode_key: str
     jwt_algorithm: str = "HS256"
-    jwt_token_expire_minutes: int = 1  # TODO: Longer expiration time
+    jwt_token_expire_minutes: int = 1440  # 24 hours
 
 
 settings = Settings()
